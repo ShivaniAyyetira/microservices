c4f8a4372c05ee370ecda7f3fa6286d79e686651
kaushik.kn <kaushikkoojugodu@gmail.com>
2022-12-29 08:52:25 +0530

    API-GATEWAY

--- currency-conversion-service/src/main/java/com/kaushik/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/kaushik/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -4,7 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+//Need to give the name of feign client same as that of application name fo target service on properties file
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean retrieveExchangeValue
--- currency-exchange-service/src/main/java/com/kaushilk/microservices/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/kaushilk/microservices/CurrencyExchange.java
@@ -2,8 +2,10 @@ package com.kaushilk.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CurrencyExchange {
 
 	public static void main(String[] args) {
